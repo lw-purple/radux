73bf43c5b394e2e2b844b6d7a3e5173138e9d24e
wangzailong <1715587588@qq.com>
2017-05-10 15:45:14 +0800

    finsh

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
     return [
-        {title:'Javascript:The Good Parts'},
-        {title:'Harry Potter'},
-        {title:'The Dark Tork'},
-        {title:'Long'}
+        {title:'Javascript:The Good Parts',pages:101},
+        {title:'Harry Potter',pages:39},
+        {title:'The Dark Tork',pages:85},
+        {title:'Long',pages:1}
     ]
 }
